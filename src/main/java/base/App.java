74cc2143f97d8f89a18fd8e44b4da459358de466
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 06:43:47 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
+package base;
+/*
+Example Output
+
+How many people? 8
+How many pizzas do you have? 2
+How many slices per pizza? 8
+8 people with 2 pizzas (16 slices)
+Each person gets 2 pieces of pizza.
+There are 0 leftover pieces.
+
+Challenges
+Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+Alter the output so it handles pluralization properly, for example: "Each person gets 2 pieces of pizza." or "Each person gets 1 piece of pizza." Handle the output for leftover pieces appropriately as well.
+Create a variant of the program that prompts for the number of people and the number of pieces each person wants, and calculate how many full pizzas you need to purchase.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
